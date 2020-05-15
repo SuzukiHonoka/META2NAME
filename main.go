a8fb93c3ba6a6f8c3f9de945ba3ba78fee480d14
SuzukiHonoka <1787074172@qq.com>
2020-05-15 23:10:54 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dhowden/tag"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	scanALL = true
+)
+
+var music = []string{"mp3","flac","wav"}
+
+func main() {
+	cwd,_ := os.Getwd()
+	audios,_ := ioutil.ReadDir(cwd)
+	for _,f := range audios {
+		name := f.Name()
+		if !scanALL {
+			if isvalid("music",name) {
+				continue
+			}
+		}
+		data,_ := os.Open(name)
+		m,err1 := tag.ReadFrom(data)
+		if err1 != nil {
+			fmt.Println(name,"Not a audio file.")
+			continue
+		}
+		abss := filepath.Join(cwd,name)
+		dname := ""
+		if !scanALL {
+			dname = filepath.Join(cwd,m.Artist()+" - "+m.Title()+filepath.Ext(name))
+		} else {
+			dname = filepath.Join(cwd,m.Artist() + " - " + m.Title() + "." + strings.ToLower(string(m.FileType())) )
+		}
+		err2 := os.Rename(abss,dname)
+		if err2 == nil{
+			fmt.Println(name,"renamed to",dname)
+		}
+		_ = data.Close()
+	}
+}
+
+func isvalid(ext string,name string)bool {
+	fext := filepath.Ext(name)
+	if len(fext) > 1 {
+		lext := strings.ToLower(fext[1:])
+		if ext == "music" {
+			for _,f := range music {
+				return lext == f
+			}
+		}
+	}
+	return false
+}
+
