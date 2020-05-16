98e77eee300b0bd9449997d5482f10439ebca813
SuzukiHonoka <1787074172@qq.com>
2020-05-16 11:06:27 +0800

    fix type check.

--- main.go
+++ main.go
@@ -13,49 +13,51 @@ const (
 	scanALL = true
 )
 
-var music = []string{"mp3","flac","wav"}
+var music = []string{"mp3", "flac", "wav"}
 
 func main() {
-	cwd,_ := os.Getwd()
-	audios,_ := ioutil.ReadDir(cwd)
-	for _,f := range audios {
+	cwd, _ := os.Getwd()
+	audios, _ := ioutil.ReadDir(cwd)
+	for _, f := range audios {
 		name := f.Name()
 		if !scanALL {
-			if isvalid("music",name) {
+			if isvalid("music", name) {
 				continue
 			}
 		}
-		data,_ := os.Open(name)
-		m,err1 := tag.ReadFrom(data)
+		data, _ := os.Open(name)
+		m, err1 := tag.ReadFrom(data)
 		if err1 != nil {
-			fmt.Println(name,"Not a audio file.")
+			fmt.Println(name, "Not a audio file.")
 			continue
 		}
-		abss := filepath.Join(cwd,name)
+		abss := filepath.Join(cwd, name)
 		dname := ""
 		if !scanALL {
-			dname = filepath.Join(cwd,m.Artist()+" - "+m.Title()+filepath.Ext(name))
+			dname = filepath.Join(cwd, m.Artist()+" - "+m.Title()+filepath.Ext(name))
 		} else {
-			dname = filepath.Join(cwd,m.Artist() + " - " + m.Title() + "." + strings.ToLower(string(m.FileType())) )
+			dname = filepath.Join(cwd, m.Artist()+" - "+m.Title()+"."+strings.ToLower(string(m.FileType())))
 		}
-		err2 := os.Rename(abss,dname)
-		if err2 == nil{
-			fmt.Println(name,"renamed to",dname)
+		err2 := os.Rename(abss, dname)
+		if err2 == nil {
+			fmt.Println(name, "renamed to", dname)
 		}
 		_ = data.Close()
 	}
 }
 
-func isvalid(ext string,name string)bool {
+func isvalid(ext string, name string) bool {
 	fext := filepath.Ext(name)
 	if len(fext) > 1 {
 		lext := strings.ToLower(fext[1:])
 		if ext == "music" {
-			for _,f := range music {
-				return lext == f
+			for _, f := range music {
+				if lext != f {
+					continue
+				}
+				return true
 			}
 		}
 	}
 	return false
 }
-
